083309fa737e770b38eb9ecbd310632190b73daf
jrosen081 <jrosen081@gmail.com>
2021-02-20 12:37:16 -0500

    Added campsite object model

---
+++ CampingAR/CampingAR/CampsiteObject.swift
@@ -0,0 +1,23 @@
+//
+//  CampingModel.swift
+//  CampingAR
+//
+//  Created by Jack Rosen on 2/20/21.
+//
+
+import Foundation
+import SwiftUI
+
+struct CampsiteObject {
+    let iconName: String
+    let entityName: String
+    let boundingBox: BoundingBox
+    let color: Color
+    let id = UUID()
+}
+
+struct BoundingBox {
+    let height: Double
+    let width: Double
+    let length: Double
+}
