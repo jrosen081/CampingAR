e79d8a5b649798fd200c32179ac8edcfe1d852a4
jrosen081 <jrosen081@gmail.com>
2021-02-20 17:13:46 -0500

    Continued working on menu

--- CampingAR/CampingAR/ContentView.swift
+++ CampingAR/CampingAR/ContentView.swift
@@ -13,19 +13,35 @@ struct ContentView : View {
     @State var totalRayTraceHits = 0
     @State var dropLocation: CGPoint? = nil
     @State var shouldShowMenu = false
-    @State var selectedObject: CampsiteObject = CampsiteObject(iconName: "", entityName: "", boundingBox: BoundingBox(height: 10, width: 10, length: 10), color: .gray)
-    let allOptions = ["fire1"]
+    @State var selectedObject: CampsiteObject? = nil
+    @State var isShowingCustomization = false
+    let allOptions = ["fire1": CampsiteObject(iconName: "Fire-Filled", entityName: "", boundingBox: BoundingBox(height: 10, width: 20, length: 10), color: .gray), "fire2": CampsiteObject(iconName: "Fire-Unfilled", entityName: "ugh", boundingBox: BoundingBox(height: 10, width: 10, length: 10), color: .gray)]
     
     
     var body: some View {
-        ZStack(alignment: .bottomTrailing) {
-            ARViewContainer(totalRayTraceHits: self.$totalRayTraceHits, dropLocation: self.$dropLocation, shouldShowMenu: self.$shouldShowMenu)
-                .becomeDroppable()
-                .edgesIgnoringSafeArea(.all)
-                .overlay(Text("Total hits: \(totalRayTraceHits)").padding(), alignment: .topTrailing)
-            if shouldShowMenu {
-                CampingObjectView()
+        ZStack(alignment: .topLeading) {
+            ZStack(alignment: .bottomTrailing) {
+                ARViewContainer(totalRayTraceHits: self.$totalRayTraceHits, dropLocation: self.$dropLocation, shouldShowMenu: self.$shouldShowMenu)
+                    .becomeDroppable()
+                    .edgesIgnoringSafeArea(.all)
+                    .overlay(Text("Total hits: \(totalRayTraceHits)").padding(), alignment: .topTrailing)
+                if shouldShowMenu {
+                    HStack {
+                        CampingObjectView()
+                        Button {
+                            self.isShowingCustomization = true
+                        } label: {
+                            Text("Test")
+                        }
+                    }
+                    
+                }
             }
+            if let object = selectedObject {
+                Image(object.iconName).frame(width: 100, height: 100)
+            }
+        }.sheet(isPresented: self.$isShowingCustomization) {
+            CustomizationView(selectedObject: self.$selectedObject, allOptions: allOptions)
         }
     }
 }
--- CampingAR/CampingAR/CustomizationView.swift
+++ CampingAR/CampingAR/CustomizationView.swift
@@ -9,23 +9,44 @@ import Foundation
 import SwiftUI
 
 struct CustomizationView: View {
-    @Binding var selectedObject: CampsiteObject
+    @Binding var selectedObject: CampsiteObject?
     
     let allOptions: [String: CampsiteObject]
     @State var currentOptions = ""
     
+    var validObjects: [(String, CampsiteObject)] {
+        allOptions.map { ($0.key, $0.value) }.sorted(by: { $0.0 < $1.0 }).filter { string, _ in
+            return string.lowercased().contains(currentOptions.lowercased()) || currentOptions.isEmpty
+        }
+    }
+    
     var body: some View {
-        TextField("Search:", text: self.$currentOptions)
-            .textFieldStyle(RoundedBorderTextFieldStyle())
-        LazyVGrid(columns: [GridItem(.fixed(150), spacing: nil, alignment: nil)]) {
-            ForEach(allOptions.map { ($0.key, $0.value) }.sorted(by: { $0.0 < $1.0 }), id: \.0) { name, object in
-                VStack {
-                    Text(name)
-                    Image(systemName: object.iconName)
-                }.border(Color.gray, width: 1).onTapGesture {
-                    self.selectedObject = object
+        VStack {
+            TextField("Search:", text: self.$currentOptions)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            LazyVGrid(columns: Array(repeating: GridItem(.flexible(minimum: 0, maximum: .infinity), spacing: nil, alignment: nil), count: 3)) {
+                ForEach(validObjects, id: \.0) { name, object in
+                    HStack {
+                        Spacer()
+                        VStack {
+                            Spacer()
+                            Text(name)
+                            Image(object.iconName).resizable().frame(maxHeight: 100)
+                            Spacer()
+                            
+                        }
+                        Spacer()
+                    }.overlay(self.rectangle)
+                    .padding().onTapGesture {
+                        self.selectedObject = object
+                    }
                 }
             }
-        }
+            Spacer()
+        }.padding()
+    }
+    
+    var rectangle: some View {
+        RoundedRectangle(cornerRadius: 10).stroke(Color.gray)
     }
 }
